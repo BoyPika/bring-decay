4f7e647a73f53f2c53f4f6eca785ef5175503ad2
BoyPika <boypika85@gmail.com>
2024-01-26 18:38:51 -0500

    add config stuff

--- src/main/java/net/boypika/bring_decay/Bring_Decay.java
+++ src/main/java/net/boypika/bring_decay/Bring_Decay.java
@@ -1,20 +1,46 @@
 package net.boypika.bring_decay;
 
+import io.github.timecubed.tulip.TulipConfigManager;
 import net.boypika.bring_decay.util.Trades;
 import net.fabricmc.api.ModInitializer;
 import net.boypika.bring_decay.potion.ModPotions;
+import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Bring_Decay implements ModInitializer {
-	public static final String MOD_ID = "bring_decay";
-	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+    public static final String MOD_ID = "bring_decay";
+    public static TulipConfigManager tulipInstance = new TulipConfigManager(MOD_ID, false);
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+    public static final Logger LOGGER1 = LogManager.getLogger("Tulip");
+    private static final String ConfigFile = FabricLoader.getInstance().getConfigDir().toString() + "\\" + MOD_ID + ".properties";
+    private boolean fileExists() {
+        File file = new File(ConfigFile);
+
+        return file.exists();
+    }
 
     @Override
     public void onInitialize() {
+        if (!fileExists()) {
+            try {
+                tulipInstance.createFileWithProperties();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        tulipInstance.saveProperty("witch_throw_wither", false);
+        tulipInstance.saveProperty("min_health_for_decay_from_witch", 8.0F);
+        tulipInstance.load();
         ModPotions.registerPotions();
-        Trades.registermytrades();
-        Bring_Decay.LOGGER.info("[1.16.3 - 1.19.2] Bring Decay Init");
-
+        Trades.registerTrades();
+        LOGGER.info("[1.16.3 - 1.19.2] Bring Decay Init");
+        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
+            LOGGER.info(String.valueOf(tulipInstance.getBoolean("witch_throw_wither")));
+            LOGGER.info(String.valueOf(tulipInstance.getFloat("min_health_for_decay_from_witch")));
+        }
     }
 }
--- src/main/java/net/boypika/bring_decay/mixin/WitchEntityMixin.java
+++ src/main/java/net/boypika/bring_decay/mixin/WitchEntityMixin.java
@@ -14,6 +14,7 @@ import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionUtil;
 import net.minecraft.potion.Potions;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -27,7 +28,7 @@ public class WitchEntityMixin {
             double d = target.getX() + vec3d.x - witchEntity.getX();
             double e = target.getEyeY() - 1.100000023841858 - witchEntity.getY();
             double f = target.getZ() + vec3d.z - witchEntity.getZ();
-            double g = Math.sqrt(d * d + f * f);
+            float g = MathHelper.sqrt(d * d + f * f);
             Potion potion = Potions.HARMING;
             if (target instanceof RaiderEntity) {
                 if (target.getHealth() <= 4.0F) {
@@ -37,20 +38,20 @@ public class WitchEntityMixin {
                 }
 
                 witchEntity.setTarget((LivingEntity)null);
-            } else if (g >= 8.0 && !target.hasStatusEffect(StatusEffects.SLOWNESS)) {
+            } else if (g >= 8.0F && !target.hasStatusEffect(StatusEffects.SLOWNESS)) {
                 potion = Potions.SLOWNESS;
             } else if (target.getHealth() >= Bring_Decay.tulipInstance.getFloat("min_health_for_decay_from_witch") && !target.hasStatusEffect(StatusEffects.WITHER) && Bring_Decay.tulipInstance.getBoolean("witch_throw_wither")) {
                 potion = ModPotions.DECAY_POTION;
             } else if (!Bring_Decay.tulipInstance.getBoolean("witch_throw_wither") && target.getHealth() >= 8.0F && !target.hasStatusEffect(StatusEffects.POISON)) {
                 potion = Potions.POISON;
-            } else if (g <= 3.0 && !target.hasStatusEffect(StatusEffects.WEAKNESS) && witchEntity.getRandom().nextFloat() < 0.25F) {
+            } else if (g <= 3.0F && !target.hasStatusEffect(StatusEffects.WEAKNESS) && witchEntity.getRandom().nextFloat() < 0.25F) {
                 potion = Potions.WEAKNESS;
             }
 
             PotionEntity potionEntity = new PotionEntity(witchEntity.world, witchEntity);
             potionEntity.setItem(PotionUtil.setPotion(new ItemStack(Items.SPLASH_POTION), potion));
-            potionEntity.setPitch(potionEntity.getPitch() - -20.0F);
-            potionEntity.setVelocity(d, e + g * 0.2, f, 0.75F, 8.0F);
+            potionEntity.pitch -= -20.0F;
+            potionEntity.setVelocity(d, e + (double)(g * 0.2F), f, 0.75F, 8.0F);
             if (!witchEntity.isSilent()) {
                 witchEntity.world.playSound((PlayerEntity)null, witchEntity.getX(), witchEntity.getY(), witchEntity.getZ(), SoundEvents.ENTITY_WITCH_THROW, witchEntity.getSoundCategory(), 1.0F, 0.8F + witchEntity.getRandom().nextFloat() * 0.4F);
             }
